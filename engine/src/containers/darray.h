2205a3e0ef69a21a2ece4cb0ea4f0e59399b808b
Alexandr Gromov <alex.gromov.2410@gmail.com>
2023-09-12 01:58:48 -0700

    Added Event system & darray

---
+++ engine/src/containers/darray.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "defines.h"
+
+/*
+Memory layout
+u64 capacity = number elements that can be held
+u64 length = number of elements currently contained
+u64 stride = size of each element in bytes
+void* elements
+*/
+
+enum
+{
+    DARRAY_CAPACITY,
+    DARRAY_LENGTH,
+    DARRAY_STRIDE,
+    DARRAY_FIELD_LENGTH
+};
+
+BAPI void* _darray_create(u64 length, u64 stride);
+BAPI void _darray_destroy(void* array);
+
+BAPI u64 _darray_field_get(void* array, u64 field);
+BAPI void _darray_field_set(void* array, u64 field, u64 value);
+
+BAPI void* _darray_resize(void* array);
+
+BAPI void* _darray_push(void* array, const void* value_ptr);
+BAPI void _darray_pop(void* array, void* dest);
+
+BAPI void* _darray_pop_at(void* array, u64 index, void* dest);
+BAPI void* _darray_insert_at(void* array, u64 index, void* value_ptr);
+
+#define DARRAY_DEFAULT_CAPACITY 1
+#define DARRAY_RESIZE_FACTOR 2
+
+#define darray_create(type) _darray_create(DARRAY_DEFAULT_CAPACITY, sizeof(type))
+
+#define darray_reserve(type, capacity) _darray_create(capacity, sizeof(type))
+
+#define darray_destroy(array) _darray_destroy(array);
+
+#define darray_push(array, value)                       \
+    {                                                   \
+        typeof(value) temp = value;                     \
+        array = _darray_push(array, &temp);             \
+    }
+
+#define darray_pop(array, value_ptr) _darray_pop(array, value_ptr)
+
+#define darray_insert_at(array, index, value)           \
+    {                                                   \
+        typeof(value) temp = value;                     \
+        array = _darray_insert_at(array, index, &temp); \
+    }
+
+#define darray_pop_at(array, index, value_ptr) _darray_pop_at(array, index, value_ptr)
+
+#define darray_clear(array) _darray_field_set(array, DARRAY_LENGTH, 0)
+
+#define darray_capacity(array) _darray_field_get(array, DARRAY_CAPACITY)
+
+#define darray_length(array) _darray_field_get(array, DARRAY_LENGTH)
+
+#define darray_stride(array) _darray_field_get(array, DARRAY_STRIDE)
+
+#define darray_length_set(array, value) _darray_field_set(array, DARRAY_LENGTH, value)
--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -3,6 +3,7 @@
 #include "logger.h"
 #include "platform/platform.h"
 #include "core/bmemory.h"
+#include "core/event.h"
 
 typedef struct application_state
 {
@@ -41,6 +42,12 @@ b8 application_create(game* game_inst)
 
     app_state.is_running = TRUE;
     app_state.is_suspended = FALSE;
+
+    if (!event_initialize())
+    {
+        BERROR("Event system failed initialization. Application cannot continue)");
+        return FALSE;
+    }
 
     if (!platform_startup(
         &app_state.platform,
@@ -94,6 +101,8 @@ b8 application_run()
     }
 
     app_state.is_running = FALSE;
+
+    event_shutdown();
 
     platform_shutdown(&app_state.platform);
     return TRUE;
