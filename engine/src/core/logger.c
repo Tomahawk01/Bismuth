4356976a9b5d02f64b518eeca834e4613b6eed5a
Alexandr Gromov <alex.gromov.2410@gmail.com>
2023-09-15 17:09:48 -0700

    Logger now creates file console.log in bin folder

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,18 +1,30 @@
 #include "logger.h"
 #include "asserts.h"
 #include "platform/platform.h"
+#include "platform/filesystem.h"
+#include "core/bstring.h"
+#include "core/bmemory.h"
 
 // TODO: TEMPORARY
-#include <stdio.h>
-#include <string.h>
 #include <stdarg.h>
 
 typedef struct logger_system_state
 {
-    b8 initialized;
+    file_handle log_file_handle;
 } logger_system_state;
 
 static logger_system_state* state_ptr;
+
+void append_to_log_file(const char* message)
+{
+    if (state_ptr && state_ptr->log_file_handle.is_valid)
+    {
+        u64 length = string_length(message);
+        u64 written = 0;
+        if (!filesystem_write(&state_ptr->log_file_handle, length, message, &written))
+            platform_console_write_error("ERROR writing to console.log", LOG_LEVEL_ERROR);
+    }
+}
 
 b8 initialize_logging(u64* memory_requirment, void* state)
 {
@@ -21,7 +33,13 @@ b8 initialize_logging(u64* memory_requirment, void* state)
         return true;
 
     state_ptr = state;
-    state_ptr->initialized = true;
+
+    // Create new/wipe existing log file, then open it
+    if (!filesystem_open("console.log", FILE_MODE_WRITE, false, &state_ptr->log_file_handle))
+    {
+        platform_console_write_error("ERROR: Unable to open console.log for writing", LOG_LEVEL_ERROR);
+        return false;
+    }
 
     // TODO: Remove this later
     BFATAL("Test message: %f", 3.14f);
@@ -31,37 +49,40 @@ b8 initialize_logging(u64* memory_requirment, void* state)
     BDEBUG("Test message: %f", 3.14f);
     BTRACE("Test message: %f", 3.14f);
 
-    // TODO: create log file.
+    // TODO: create log file
     return true;
 }
 
 void shutdown_logging(void* state)
 {
-    // TODO: cleanup logging/write queued entries.
+    // TODO: cleanup logging/write queued entries
     state_ptr = 0;
 }
 
 void log_output(log_level level, const char* message, ...)
 {
+    // TODO: String operations are slow. Move it to another thread later
     const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]:  ", "[INFO]:  ", "[DEBUG]: ", "[TRACE]: "};
     b8 is_error = level < LOG_LEVEL_WARN;
 
-    const i32 msg_length = 32000;
-    char out_message[msg_length];
-    memset(out_message, 0, sizeof(out_message));
+    char out_message[32000];
+    bzero_memory(out_message, sizeof(out_message));
 
     __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, msg_length, message, arg_ptr);
+    string_format_v(out_message, message, arg_ptr);
     va_end(arg_ptr);
 
-    char out_message2[msg_length];
-    sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
+    // Prepend log level to message
+    string_format(out_message, "%s%s\n", level_strings[level], out_message);
 
     if (is_error)
-        platform_console_write_error(out_message2, level);
+        platform_console_write_error(out_message, level);
     else
-        platform_console_write(out_message2, level);
+        platform_console_write(out_message, level);
+    
+    // Queue copy to be written to the log file
+    append_to_log_file(out_message);
 }
 
 void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line)
