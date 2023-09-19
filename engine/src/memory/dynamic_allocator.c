9d0b75620dc68934acf30b5deb3d357170750d8e
Alexandr Gromov <alex.gromov.2410@gmail.com>
2023-09-19 12:30:26 -0700

    Added Dynamic allocator & tests
    - freelist fix

---
+++ engine/src/memory/dynamic_allocator.c
@@ -0,0 +1,122 @@
+#include "dynamic_allocator.h"
+#include "core/bmemory.h"
+#include "core/logger.h"
+#include "containers/freelist.h"
+
+typedef struct dynamic_allocator_state
+{
+    u64 total_size;
+    freelist list;
+    void* freelist_block;
+    void* memory_block;
+} dynamic_allocator_state;
+
+b8 dynamic_allocator_create(u64 total_size, u64* memory_requirement, void* memory, dynamic_allocator* out_allocator)
+{
+    if (total_size < 1)
+    {
+        BERROR("dynamic_allocator_create cannot have a total_size of 0. Create failed");
+        return false;
+    }
+    if (!memory_requirement)
+    {
+        BERROR("dynamic_allocator_create requires memory_requirement to exist. Create failed");
+        return false;
+    }
+    u64 freelist_requirement = 0;
+    // Grab memory requirement for the free list first
+    freelist_create(total_size, &freelist_requirement, 0, 0);
+
+    *memory_requirement = freelist_requirement + sizeof(dynamic_allocator_state) + total_size;
+
+    // If only obtaining requirement, boot out
+    if (!memory)
+        return true;
+
+    // Memory layout:
+    // state
+    // freelist block
+    // memory block
+    out_allocator->memory = memory;
+    dynamic_allocator_state* state = out_allocator->memory;
+    state->total_size = total_size;
+    state->freelist_block = (void*)(out_allocator->memory + sizeof(dynamic_allocator_state));
+    state->memory_block = (void*)(state->freelist_block + freelist_requirement);
+
+    // Create freelist
+    freelist_create(total_size, &freelist_requirement, state->freelist_block, &state->list);
+
+    bzero_memory(state->memory_block, total_size);
+    return true;
+}
+
+b8 dynamic_allocator_destroy(dynamic_allocator* allocator)
+{
+    if (allocator)
+    {
+        dynamic_allocator_state* state = allocator->memory;
+        freelist_destroy(&state->list);
+        bzero_memory(state->memory_block, state->total_size);
+        state->total_size = 0;
+        allocator->memory = 0;
+        return true;
+    }
+
+    BWARN("dynamic_allocator_destroy requires a pointer to an allocator. Destroy failed");
+    return false;
+}
+
+void* dynamic_allocator_allocate(dynamic_allocator* allocator, u64 size)
+{
+    if (allocator && size)
+    {
+        dynamic_allocator_state* state = allocator->memory;
+        u64 offset = 0;
+        // Attempt to allocate from freelist
+        if (freelist_allocate_block(&state->list, size, &offset))
+        {
+            // Use that offset against base memory block to get the block
+            void* block = (void*)(state->memory_block + offset);
+            return block;
+        }
+        else
+        {
+            BERROR("dynamic_allocator_allocate no blocks of memory large enough to allocate from");
+            u64 available = freelist_free_space(&state->list);
+            BERROR("Requested size: %llu, total space available: %llu", size, available);
+            return 0;
+        }
+    }
+
+    BERROR("dynamic_allocator_allocate requires a valid allocator and size");
+    return 0;
+}
+
+void dynamic_allocator_free(dynamic_allocator* allocator, void* block, u64 size)
+{
+    if (!allocator || !block)
+    {
+        BERROR("dynamic_allocator_free requires both a valid allocator (0x%p) and a block (0x%p) to be freed", allocator, block);
+        return;
+    }
+
+    dynamic_allocator_state* state = allocator->memory;
+    if (block < state->memory_block || block > state->memory_block + state->total_size)
+    {
+        void* end_of_block = (void*)(state->memory_block + state->total_size);
+        BERROR("dynamic_allocator_free trying to release block (0x%p) outside of allocator range (0x%p)-(0x%p)", block, state->memory_block, end_of_block);
+        return;
+    }
+    u64 offset = (block - state->memory_block);
+    if (!freelist_free_block(&state->list, size, offset))
+    {
+        BERROR("dynamic_allocator_free failed");
+        return;
+    }
+}
+
+u64 dynamic_allocator_free_space(dynamic_allocator* allocator)
+{
+    dynamic_allocator_state* state = allocator->memory;
+    return freelist_free_space(&state->list);
+}
--- tests/src/main.c
+++ tests/src/main.c
@@ -2,6 +2,7 @@
 #include "memory/linear_allocator_tests.h"
 #include "containers/hashtable_tests.h"
 #include "containers/freelist_tests.h"
+#include "memory/dynamic_allocator_tests.h"
 
 #include <core/logger.h>
 
@@ -13,6 +14,7 @@ int main()
     linear_allocator_register_tests();
     hashtable_register_tests();
     freelist_register_tests();
+    dynamic_allocator_register_tests();
 
     BDEBUG("Starting tests...");
 
