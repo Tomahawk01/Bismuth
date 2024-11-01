047267f2dcfde7d20bf763bafd4e78b743f6047c
Alexandr Gromov <66443586+Tomahawk01@users.noreply.github.com>
2024-11-01 10:51:24 -0400

    WIP bactor

--- bismuth.runtime/src/world/bactor.c
+++ bismuth.runtime/src/world/bactor.c
@@ -3,6 +3,8 @@
 #include "bresources/bresource_types.h"
 #include "math/geometry.h"
 #include <debug/bassert.h>
+#include <logger.h>
+#include <memory/bmemory.h>
 
 typedef struct bactor_staticmesh_system_state
 {
@@ -40,29 +42,93 @@ void bactor_comp_staticmesh_system_shutdown(struct bactor_staticmesh_system_stat
     if (state)
     {
         // TODO: things
+        bzero_memory(state, sizeof(bactor_staticmesh_system_state));
     }
 }
 
-u64 bactor_comp_staticmesh_create(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name, geometry g, bresource_material_instance material)
+static u32 get_free_index(struct bactor_staticmesh_system_state* state)
+{
+    if (!state)
+        return INVALID_ID;
+
+    for (u32 i = 0; i < state->max_components; ++i)
+    {
+        if (state->materials[i].per_draw_id == INVALID_ID && state->geometries[i].id == INVALID_ID)
+            return i;
+    }
+
+    BERROR("Failed to find free slot for static mesh load. Increase system config->max_components. Current=%u", state->max_components);
+    return INVALID_ID;
+}
+
+u32 bactor_comp_staticmesh_create(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name, geometry g, bresource_material_instance material)
+{
+    u32 index = get_free_index(state);
+    if (index != INVALID_ID)
+    {
+        state->geometries[index] = g;
+        state->materials[index] = material;
+    }
+
+    return index;
+}
+
+u32 bactor_comp_staticmesh_get_id(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name)
 {
 }
 
-u64 bactor_comp_staticmesh_get_id(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name)
+void bactor_comp_staticmesh_destroy(struct bactor_staticmesh_system_state* state, u32 id)
 {
 }
 
-void bactor_comp_staticmesh_destroy(struct bactor_staticmesh_system_state* state, u64 id)
+b8 bactor_comp_staticmesh_load(u32 actor_id)
 {
 }
 
-b8 bactor_comp_staticmesh_get_geometry(struct bactor_staticmesh_system_state* state, u64 id, geometry* out_geometry)
+b8 bactor_comp_staticmesh_unload(u32 actor_id)
 {
 }
 
-b8 bactor_comp_staticmesh_get_material(struct bactor_staticmesh_system_state* state, u64 id, bresource_material_instance* out_material)
+geometry* bactor_comp_staticmesh_get_geometry(struct bactor_staticmesh_system_state* state, u32 id)
 {
+    if (!state || id == INVALID_ID)
+        return 0;
+
+    return &state->geometries[id];
 }
 
-b8 bactor_comp_staticmesh_get_geometry_material(struct bactor_staticmesh_system_state* state, u64 id, geometry* out_geometry, bresource_material_instance* out_material)
+bresource_material_instance* bactor_comp_staticmesh_get_material(struct bactor_staticmesh_system_state* state, u32 id)
 {
+    if (!state || id == INVALID_ID)
+        return 0;
+
+    return &state->materials[id];
+}
+
+b8 bactor_comp_staticmesh_get_geometry_material(struct bactor_staticmesh_system_state* state, u32 id, geometry** out_geometry, bresource_material_instance** out_material)
+{
+    if (!state || id == INVALID_ID)
+        return false;
+
+    *out_material = &state->materials[id];
+    *out_geometry = &state->geometries[id];
+
+    return true;
+}
+
+b8 bactor_comp_staticmesh_get_render_data(struct bactor_staticmesh_system_state* state, u32 id, struct bactor_comp_staticmesh_render_data* out_render_data)
+{
+    if (!state || !out_render_data || id == INVALID_ID)
+        return false;
+
+    out_render_data->material = &state->materials[id];
+
+    geometry* g = &state->geometries[id];
+
+    out_render_data->vertex_count = g->vertex_count;
+    out_render_data->vertex_buffer_offset = g->vertex_buffer_offset;
+    out_render_data->index_count = g->index_count;
+    out_render_data->index_buffer_offset = g->index_buffer_offset;
+
+    return true;
 }
--- bismuth.runtime/src/world/bactor.h
+++ bismuth.runtime/src/world/bactor.h
@@ -26,13 +26,53 @@ typedef struct bactor_staticmesh_system_config
     u32 max_components;
 } bactor_staticmesh_system_config;
 
+typedef enum staticmesh_render_data_flag
+{
+    /** @brief Indicates that the winding order for the given static mesh should be inverted */
+    STATICMESH_RENDER_DATA_FLAG_WINDING_INVERTED_BIT = 0x0001
+} staticmesh_render_data_flag;
+
+/** @brief Collection of flags for a static mesh to be rendered */
+typedef u32 staticmesh_render_data_flag_bits;
+
+typedef struct bactor_comp_staticmesh_render_data
+{
+    const bresource_material_instance* material;
+
+    u64 unique_id;
+
+    /** @brief Flags for the static mesh to be rendered */
+    staticmesh_render_data_flag_bits flags;
+
+    /** @brief Additional tint to be applied to the static mesh when rendered */
+    vec4 tint;
+
+    /** @brief The vertex count */
+    u32 vertex_count;
+    /** @brief The offset from the beginning of the vertex buffer */
+    u64 vertex_buffer_offset;
+
+    /** @brief The index count */
+    u32 index_count;
+    /** @brief The offset from the beginning of the index buffer */
+    u64 index_buffer_offset;
+
+    /** @brief The index of the IBL probe to use */
+    u32 ibl_probe_index;
+} bactor_comp_staticmesh_render_data;
+
 BAPI b8 bactor_comp_staticmesh_system_initialize(u64* memory_requirement, void* state, const bactor_staticmesh_system_config* config);
 BAPI void bactor_comp_staticmesh_system_shutdown(struct bactor_staticmesh_system_state* state);
 
-BAPI u64 bactor_comp_staticmesh_create(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name, geometry g, bresource_material_instance material);
-BAPI u64 bactor_comp_staticmesh_get_id(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name);
-BAPI void bactor_comp_staticmesh_destroy(struct bactor_staticmesh_system_state* state, u64 id);
+BAPI u32 bactor_comp_staticmesh_create(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name, geometry g, bresource_material_instance material);
+BAPI u32 bactor_comp_staticmesh_get_id(struct bactor_staticmesh_system_state* state, u64 actor_id, bname name);
+BAPI void bactor_comp_staticmesh_destroy(struct bactor_staticmesh_system_state* state, u32 id);
 
-BAPI b8 bactor_comp_staticmesh_get_geometry(struct bactor_staticmesh_system_state* state, u64 id, geometry* out_geometry);
-BAPI b8 bactor_comp_staticmesh_get_material(struct bactor_staticmesh_system_state* state, u64 id, bresource_material_instance* out_material);
-BAPI b8 bactor_comp_staticmesh_get_geometry_material(struct bactor_staticmesh_system_state* state, u64 id, geometry* out_geometry, bresource_material_instance* out_material);
+BAPI b8 bactor_comp_staticmesh_load(u32 actor_id);
+BAPI b8 bactor_comp_staticmesh_unload(u32 actor_id);
+
+BAPI geometry* bactor_comp_staticmesh_get_geometry(struct bactor_staticmesh_system_state* state, u32 id);
+BAPI bresource_material_instance* bactor_comp_staticmesh_get_material(struct bactor_staticmesh_system_state* state, u32 id);
+BAPI b8 bactor_comp_staticmesh_get_geometry_material(struct bactor_staticmesh_system_state* state, u32 id, geometry** out_geometry, bresource_material_instance** out_material);
+
+BAPI b8 bactor_comp_staticmesh_get_render_data(struct bactor_staticmesh_system_state* state, u32 id, struct bactor_comp_staticmesh_render_data* out_render_data);
